1b510ace01fefcf2e651063398e5066687c122ab
Daryl Stark <daryl@dstark.nl>
2024-11-01 12:30:52 +0000

    Add `rtd` theme

--- source/conf.py
+++ source/conf.py
@@ -6,23 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MBorg BS Docs'
-copyright = '2024, Daryl Stark'
-author = 'Daryl Stark'
-release = '1.0.0'
+project = "MBorg BS Docs"
+copyright = "2024, Daryl Stark"
+author = "Daryl Stark"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'nl'
+language = "nl"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
